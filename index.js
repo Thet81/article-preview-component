aab578a2922ef872ac71f02a19c7ce1576c55f5d
thetnaing <thetnaingoo1530@gmail.com>
2026-05-31 22:16:57 +0630

    implemented to control the css with js

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
 // index.js
  
- const button = document.getElementById('share-btn')
- const div = document.getElementById("final-div")
- console.log(button)
+ const shareButton = document.getElementById('share-btn')
+ const card = document.getElementById("card")
 
- button.addEventListener('click',()=> {
- 	console.log('hello')
- 	const newDiv = document.createElement("div")
- 	const para = document.createElement("p")
- 	para.textContent = "someparagraph"
- 	newDiv.appendChild(para)
- 	newDiv.style.backgroundColor = "white"
- 	newDiv.style.padding = "1rem"
- 	// newDiv.style.marginTop = "2rem"
- 	newDiv.style.border = "1px solid red"
- 	div.replaceWith(newDiv)
+ shareButton.addEventListener('click',()=> {
+ 	card.classList.toggle("share-open")
  })
